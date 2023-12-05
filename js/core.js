8b560cb2cf2217c4b08481e2b41d6fa590d5f82d
azamatovamal5@gmail.com <elaman@outlook.com>
2023-12-05 19:17:56 +0600

    add RABOTA

---
+++ js/core.js
@@ -0,0 +1,8 @@
+document.querySelector('.js-btn').addEventListener('click', function () {
+  document.querySelector('.js-btn').classList.add('active')
+  document.querySelector('.js-btn').style.height = "200px"
+  document.querySelector('.js-btn').textContent = "Текст кнопки изменен"
+
+  let newhr = document.createElement('<p>Paragraph</p>');
+  document.querySelector('.js-btn').insertAdjacentElement('afterend, newhr');
+})
